e0c32296617e38e0786cc147bbd99e2414c4fa58
José Cristovão <j_cristovao@hotmail.com>
2023-03-21 09:23:57 +0100

    Primeiro commit

---
+++ Server/SignalRChatServer/Hub/Chat.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+
+namespace SignalRChatServer
+{
+    public class Chat: Hub
+    {
+        public static List<Message> Messages { get; set; }
+
+
+        public Chat()
+        {
+            if (Messages == null) Messages = new List<Message>();
+        }
+        public void NewMessage(string userName, string message)
+        {
+            Clients.All.SendAsync("NewMessage", userName, message);
+            Messages.Add(new Message()
+            {
+                Text = message,
+                UserName = userName
+            }) ;            
+        }
+
+        public void NewUser(string userName, string conectionID)
+        {
+            Clients.Client(conectionID).SendAsync("previousMessages", Messages);
+            Clients.All.SendAsync("newUser", userName);
+        }
+
+
+        public class Message
+        {
+            public string UserName { get; set; }
+            public string Text { get; set; }
+        }
+    }
+}
